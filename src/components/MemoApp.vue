2c9342852e26cb51dd3afc00f9d1a5b49659ba0d
bumkey <dhmk815@naver.com>
2020-03-10 00:03:28 +0900

    Delete까지

---
+++ src/components/MemoApp.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="memo-app">
+    <memo-form @addMemo="addMemo" />
+    <ul class="memo-list">
+      <memo
+        v-for="memo in memos"
+        :key="memo.id"
+        :memo="memo"
+        @deleteMemo="deleteMemo"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import MemoForm from './MemoForm'
+import Memo from './Memo'
+
+export default {
+  name: 'MemoApp',
+  components: {
+    MemoForm,
+    Memo
+  },
+  data () {
+    return {
+      memos: []
+    }
+  },
+  created () {
+    this.memos = localStorage.memos ? JSON.parse(localStorage.memos) : []
+  },
+  methods: {
+    addMemo (payload) {
+      this.memos.push(payload)
+      this.storeMemo()
+    },
+    storeMemo () {
+      const memosToString = JSON.stringify(this.memos)
+      localStorage.setItem('memos', memosToString)
+    },
+    deleteMemo (id) {
+      const targetIndex = this.memos.findIndex(v => v.id === id)
+      this.memos.splice(targetIndex, 1)
+      this.storeMemo()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.memo-list {
+  padding: 20px 0;
+  margin: 0;
+}
+</style>
